ae82eefc14cd4b099c8e44871c563231dd313ea3
tsukaryu <drgon8otsuka@gmail.com>
2022-06-24 23:48:55 +0900

    feat: all posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 import Layout from '@/components/Layout'
-import { posts } from '../../mock/posts'
+import { getAllPosts } from 'lib/api'
+import PostType from 'types/post'
 import { Posts } from '../components/posts/Posts'
 
-const Index = () => (
+type Props = {
+  allPosts: PostType[]
+}
+
+const Index = ({ allPosts }: Props) => (
   <Layout>
-    <Posts posts={posts} />
+    <Posts posts={allPosts} />
   </Layout>
 )
 
 export default Index
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -2,10 +2,12 @@ import Layout from '@/components/Layout'
 import { PostDetail } from '@/components/posts/detail/PostDetail'
 import { posts } from '../../../mock/posts'
 
-const PostPage = () => (
-  <Layout hasAboutMe={false}>
-    <PostDetail post={posts[0]} />
-  </Layout>
-)
+const PostPage = () => {
+  return (
+    <Layout hasAboutMe={false}>
+      <PostDetail post={posts[0]} />
+    </Layout>
+  )
+}
 
 export default PostPage

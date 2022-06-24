ea5c6a14d949f2f9946c044390c6db6e6193963a
tsukaryu <drgon8otsuka@gmail.com>
2022-06-25 00:13:16 +0900

    fix: convert content to string

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,8 +1,20 @@
 import Layout from '@/components/Layout'
 import { PostDetail } from '@/components/posts/detail/PostDetail'
+import { getAllPosts, getPostBySlug } from 'lib/api'
 import { posts } from '../../../mock/posts'
+import remark from 'remark'
+import html from 'remark-html'
+import gfm from 'remark-gfm'
+// @ts-ignore
+import prism from 'remark-prism'
+import PostType from '../../types/post'
 
-const PostPage = () => {
+type Props = {
+  post: PostType
+}
+
+const PostPage = ({ post }: Props) => {
+  console.log({ post })
   return (
     <Layout hasAboutMe={false}>
       <PostDetail post={posts[0]} />
@@ -11,3 +23,52 @@ const PostPage = () => {
 }
 
 export default PostPage
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const post = getPostBySlug(params.slug, [
+    'title',
+    'date',
+    'slug',
+    'author',
+    'content',
+    'ogImage',
+    'coverImage',
+  ])
+  const content = await remark()
+    .use(gfm)
+    .use(prism)
+    .use(html)
+    .process(post.content)
+
+  console.log({ content })
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content: content.toString(),
+      },
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(['slug'])
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}

d46c85ce73545bb3077e346997c3b10f7065959c
tsukaryu <drgon8otsuka@gmail.com>
2022-06-24 18:45:26 +0900

    fix: footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,27 +1,12 @@
-import { Box, Flex, FlexProps, Text, Icon } from '@chakra-ui/react'
+import { Text, Icon, HStack, VStack } from '@chakra-ui/react'
 import { FiGithub, FiTwitter } from 'react-icons/fi'
 
-export const Footer = (props: FlexProps) => (
-  <Flex
-    as="footer"
-    mt="5vh"
-    py="5vh"
-    direction="column"
-    gap={{ base: '1vh', sm: '3vh' }}
-  >
-    <Flex
-      flexDirection="row"
-      justifyContent="center"
-      alignItems="center"
-      gap="2vw"
-    >
-      <Box>
-        <Icon as={FiGithub} w={5} h={5} />
-      </Box>
-      <Box>
-        <Icon as={FiTwitter} w={5} h={5} />
-      </Box>
-    </Flex>
+export const Footer = () => (
+  <VStack pt={24} pb={8}>
+    <HStack spacing={'8'}>
+      <Icon as={FiGithub} w={5} h={5} />
+      <Icon as={FiTwitter} w={5} h={5} />
+    </HStack>
     <Text>&copy; 2022 Sushin-ya. All rights reserved.</Text>
-  </Flex>
+  </VStack>
 )

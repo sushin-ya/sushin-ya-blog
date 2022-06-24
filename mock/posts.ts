96160a9aec2d09d91a5f451a2a7d80530b6512ae
tsukaryu <drgon8otsuka@gmail.com>
2022-06-24 16:45:23 +0900

    fix: empty

--- mock/posts.ts
+++ mock/posts.ts
@@ -1,43 +1,43 @@
 import PostType from '../src/types/post'
 
 export const posts: PostType[] = [
-  {
-    slug: 'dynamic-routing',
-    title: 'Dynamic Routing and Static Generation',
-    date: '2020-03-16T05:35:07.322Z',
-    coverImage: '/assets/blog/dynamic-routing/cover.jpg',
-    author: { name: 'JJ Kasper', picture: '/assets/blog/authors/jj.jpeg' },
-    excerpt:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
-    ogImage: {
-      url: '/assets/blog/dynamic-routing/cover.jpg',
-    },
-    content: 'content text',
-  },
-  {
-    slug: 'dynamic-routing',
-    title: 'Dynamic Routing and Static Generation',
-    date: '2020-03-16T05:35:07.322Z',
-    coverImage: '/assets/blog/dynamic-routing/cover.jpg',
-    author: { name: 'JJ Kasper', picture: '/assets/blog/authors/jj.jpeg' },
-    excerpt:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
-    ogImage: {
-      url: '/assets/blog/dynamic-routing/cover.jpg',
-    },
-    content: 'content text',
-  },
-  {
-    slug: 'dynamic-routing',
-    title: 'Dynamic Routing and Static Generation',
-    date: '2020-03-16T05:35:07.322Z',
-    coverImage: '/assets/blog/dynamic-routing/cover.jpg',
-    author: { name: 'JJ Kasper', picture: '/assets/blog/authors/jj.jpeg' },
-    excerpt:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
-    ogImage: {
-      url: '/assets/blog/dynamic-routing/cover.jpg',
-    },
-    content: 'content text',
-  },
+  // {
+  //   slug: 'dynamic-routing',
+  //   title: 'Dynamic Routing and Static Generation',
+  //   date: '2020-03-16T05:35:07.322Z',
+  //   coverImage: '/assets/blog/dynamic-routing/cover.jpg',
+  //   author: { name: 'JJ Kasper', picture: '/assets/blog/authors/jj.jpeg' },
+  //   excerpt:
+  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+  //   ogImage: {
+  //     url: '/assets/blog/dynamic-routing/cover.jpg',
+  //   },
+  //   content: 'content text',
+  // },
+  // {
+  //   slug: 'dynamic-routing',
+  //   title: 'Dynamic Routing and Static Generation',
+  //   date: '2020-03-16T05:35:07.322Z',
+  //   coverImage: '/assets/blog/dynamic-routing/cover.jpg',
+  //   author: { name: 'JJ Kasper', picture: '/assets/blog/authors/jj.jpeg' },
+  //   excerpt:
+  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+  //   ogImage: {
+  //     url: '/assets/blog/dynamic-routing/cover.jpg',
+  //   },
+  //   content: 'content text',
+  // },
+  // {
+  //   slug: 'dynamic-routing',
+  //   title: 'Dynamic Routing and Static Generation',
+  //   date: '2020-03-16T05:35:07.322Z',
+  //   coverImage: '/assets/blog/dynamic-routing/cover.jpg',
+  //   author: { name: 'JJ Kasper', picture: '/assets/blog/authors/jj.jpeg' },
+  //   excerpt:
+  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.',
+  //   ogImage: {
+  //     url: '/assets/blog/dynamic-routing/cover.jpg',
+  //   },
+  //   content: 'content text',
+  // },
 ]
--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,6 +1,7 @@
 import { FlexProps, List, ListItem, Text } from '@chakra-ui/react'
 import { posts } from '../../../mock/posts'
 import PostType from '../../types/post'
+import { Empty } from './Empty'
 import { Post } from './Post'
 
 type Props = {
@@ -10,13 +11,15 @@ type Props = {
 export const Posts = () => {
   return (
     <List width={{ base: '90vw', md: '768px' }} spacing={20}>
-      {posts.length > 0 &&
+      {posts.length > 0 ? (
         posts.map((post, index) => (
           <ListItem key={index}>
             <Post post={post} />
           </ListItem>
-        ))}
-      <Text>test</Text>
+        ))
+      ) : (
+        <Empty />
+      )}
     </List>
   )
 }

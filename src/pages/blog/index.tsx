8ae6d33c60f94586a9d0bf3acc39537914aa800d
tsukaryu <drgon8otsuka@gmail.com>
2022-06-25 11:35:17 +0900

    feat: blog

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,31 @@
+import Layout from '@/components/Layout'
+import { PostList } from '@/components/posts/PostList'
+import { getAllPosts } from 'lib/api'
+import PostType from 'types/post'
+
+type Props = {
+  allPosts: PostType[]
+}
+
+const Index = ({ allPosts }: Props) => (
+  <Layout hasAboutMe={false}>
+    <PostList posts={allPosts} />
+  </Layout>
+)
+
+export default Index
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}

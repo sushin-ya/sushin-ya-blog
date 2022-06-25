4b9f1671b11baeae016d0d338bdd57453b8d7e2a
tsukaryu <drgon8otsuka@gmail.com>
2022-06-25 12:00:02 +0900

    fix: link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,21 @@
-import { Text, Icon, HStack, VStack } from '@chakra-ui/react'
+import {
+  Text,
+  Icon,
+  HStack,
+  VStack,
+  Link as ChakraLink,
+} from '@chakra-ui/react'
 import { FiGithub, FiTwitter } from 'react-icons/fi'
 
 export const Footer = () => (
   <VStack pt={24} pb={8}>
     <HStack spacing={'8'}>
-      <Icon as={FiGithub} w={5} h={5} />
-      <Icon as={FiTwitter} w={5} h={5} />
+      <ChakraLink href={'https://github.com/sushin-ya'}>
+        <Icon as={FiGithub} w={5} h={5} />
+      </ChakraLink>
+      <ChakraLink href={'https://twitter.com/sushin_ya'}>
+        <Icon as={FiTwitter} w={5} h={5} />
+      </ChakraLink>
     </HStack>
     <Text>&copy; 2022 Sushin-ya. All rights reserved.</Text>
   </VStack>
